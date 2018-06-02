6330342ed580bd859770ff03a750efb9d28f43c1
Weida <weida.ma@gmail.com>
2018-06-03 00:44:57 +0800

    made it pip-installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aliyun_api_gateway_demo_sign",
+    version="0.0.1",
+    author="Alibaba",
+    description="aliyun api gateway signature",
+    license="APACHE",
+    url="https://github.com/aliyun/api-gateway-demo-sign-python",
+    packages=find_packages()
+)
